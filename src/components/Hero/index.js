f732f6c068027914d31a7e83ba8a980b1f816585
jonnebr <joaomgoncalvesbr@gmail.com>
2021-01-03 17:46:25 -0300

    logo img

--- src/components/Hero/index.js
+++ src/components/Hero/index.js
@@ -1,10 +1,14 @@
 import React from 'react'
 import './style.css'
+import logo from '../../img/Breaking_Bad_logo.svg'
 
 const Hero = () => {
   return (
     <section className="hero">
-      <div className="gallery">
+      <div className="logo-img">
+        <img src={logo} alt=""/>
+      </div>
+        <div>
         <h1>GALLERY</h1>
         <h1>GALLERY</h1>
         <h1>GALLERY</h1>
