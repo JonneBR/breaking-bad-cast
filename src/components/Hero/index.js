b84c7b8c09757bdbf68ba7e695e38d101714de7a
jonnebr <joaomgoncalvesbr@gmail.com>
2021-01-04 11:21:37 -0300

    hero centering divs

--- src/components/Hero/index.js
+++ src/components/Hero/index.js
@@ -1,63 +1,68 @@
-import React from 'react'
-import './style.css'
-import logo from '../../img/Breaking_Bad_logo.svg'
+import React from 'react';
+import './style.css';
+import logo from '../../img/Breaking_Bad_logo.svg';
 
 const Hero = () => {
   return (
     <section className="hero">
-        <div className="logo-img">
-          <img src={logo} alt="" />
+      <div className="logo-img">
+        <img src={logo} alt="" />
+      </div>
+      <div className="container">
+        <div className="center">
+          <div className="search">
+            <input type="text" />
+          </div>
+
+          <div className="gallery-container">
+            <h1>GALLERY</h1>
+            <h1>GALLERY</h1>
+            <h1>GALLERY</h1>
+            <h1>GALLERY</h1>
+            <h1>GALLERY</h1>
+            <h1>GALLERY</h1>
+            <h1>GALLERY</h1>
+            <h1>GALLERY</h1>
+            <h1>GALLERY</h1>
+            <h1>GALLERY</h1>
+            <h1>GALLERY</h1>
+            <h1>GALLERY</h1>
+            <h1>GALLERY</h1>
+            <h1>GALLERY</h1>
+            <h1>GALLERY</h1>
+            <h1>GALLERY</h1>
+            <h1>GALLERY</h1>
+            <h1>GALLERY</h1>
+            <h1>GALLERY</h1>
+            <h1>GALLERY</h1>
+            <h1>GALLERY</h1>
+            <h1>GALLERY</h1>
+            <h1>GALLERY</h1>
+            <h1>GALLERY</h1>
+            <h1>GALLERY</h1>
+            <h1>GALLERY</h1>
+            <h1>GALLERY</h1>
+            <h1>GALLERY</h1>
+            <h1>GALLERY</h1>
+            <h1>GALLERY</h1>
+            <h1>GALLERY</h1>
+            <h1>GALLERY</h1>
+            <h1>GALLERY</h1>
+            <h1>GALLERY</h1>
+            <h1>GALLERY</h1>
+            <h1>GALLERY</h1>
+            <h1>GALLERY</h1>
+            <h1>GALLERY</h1>
+            <h1>GALLERY</h1>
+            <h1>GALLERY</h1>
+            <h1>GALLERY</h1>
+            <h1>GALLERY</h1>
+            <h1>GALLERYVV</h1>
+          </div>
         </div>
-        <div className="search">
-          <input type="text" />
-        </div>
-      <div>
-        <h1>GALLERY</h1>
-        <h1>GALLERY</h1>
-        <h1>GALLERY</h1>
-        <h1>GALLERY</h1>
-        <h1>GALLERY</h1>
-        <h1>GALLERY</h1>
-        <h1>GALLERY</h1>
-        <h1>GALLERY</h1>
-        <h1>GALLERY</h1>
-        <h1>GALLERY</h1>
-        <h1>GALLERY</h1>
-        <h1>GALLERY</h1>
-        <h1>GALLERY</h1>
-        <h1>GALLERY</h1>
-        <h1>GALLERY</h1>
-        <h1>GALLERY</h1>
-        <h1>GALLERY</h1>
-        <h1>GALLERY</h1>
-        <h1>GALLERY</h1>
-        <h1>GALLERY</h1>
-        <h1>GALLERY</h1>
-        <h1>GALLERY</h1>
-        <h1>GALLERY</h1>
-        <h1>GALLERY</h1>
-        <h1>GALLERY</h1>
-        <h1>GALLERY</h1>
-        <h1>GALLERY</h1>
-        <h1>GALLERY</h1>
-        <h1>GALLERY</h1>
-        <h1>GALLERY</h1>
-        <h1>GALLERY</h1>
-        <h1>GALLERY</h1>
-        <h1>GALLERY</h1>
-        <h1>GALLERY</h1>
-        <h1>GALLERY</h1>
-        <h1>GALLERY</h1>
-        <h1>GALLERY</h1>
-        <h1>GALLERY</h1>
-        <h1>GALLERY</h1>
-        <h1>GALLERY</h1>
-        <h1>GALLERY</h1>
-        <h1>GALLERY</h1>
-        <h1>GALLERYVV</h1>
       </div>
     </section>
   );
-}
+};
 
-export default Hero
+export default Hero;
