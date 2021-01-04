bc06faec9aadb8720fa812cbd2bc35fbcf0350bf
jonnebr <joaomgoncalvesbr@gmail.com>
2021-01-04 10:40:51 -0300

    css hero search and image

--- src/components/Hero/index.js
+++ src/components/Hero/index.js
@@ -5,13 +5,13 @@ import logo from '../../img/Breaking_Bad_logo.svg'
 const Hero = () => {
   return (
     <section className="hero">
-      <div className="logo-img">
-        <img src={logo} alt=""/>
-      </div>
-      <div className="search">
-        <input type="text"/>
-      </div>
-        <div>
+        <div className="logo-img">
+          <img src={logo} alt="" />
+        </div>
+        <div className="search">
+          <input type="text" />
+        </div>
+      <div>
         <h1>GALLERY</h1>
         <h1>GALLERY</h1>
         <h1>GALLERY</h1>
@@ -57,7 +57,7 @@ const Hero = () => {
         <h1>GALLERYVV</h1>
       </div>
     </section>
-  )
+  );
 }
 
 export default Hero
