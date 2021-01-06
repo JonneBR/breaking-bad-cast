7c6571d2a7c49e09dbc3167ba1cc4b8362604742
jonnebr <joaomgoncalvesbr@gmail.com>
2021-01-06 12:11:47 -0300

    api request

--- src/App.js
+++ src/App.js
@@ -1,8 +1,24 @@
+import React, {useState, useEffect} from 'react'
 import './App.css';
 import Navbar from './components/Navbar/index';
 import Hero from './components/Hero/index';
 
+
 const App = () => {
+
+  const [items, setItems] = useState([])
+  const [isLoading, setIsLoading] = useState(true)
+
+  useEffect(() => {
+    const fetchItems = async () => {
+      const response = await fetch(`https://www.breakingbadapi.com/api/characters`)
+      const result = await response.json()
+      console.log(result);
+    }
+
+    fetchItems()
+  }, [])
+
   return (
     <div className="App">
        <Navbar/>
