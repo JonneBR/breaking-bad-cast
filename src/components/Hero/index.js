6b4e1e3499f273fe6cef518bb63839e5eb3c330d
jonnebr <joaomgoncalvesbr@gmail.com>
2021-01-07 06:02:04 -0300

    items img map working

--- src/components/Hero/index.js
+++ src/components/Hero/index.js
@@ -4,11 +4,6 @@ import logo from '../../img/Breaking_Bad_logo.svg';
 
 
 const Hero = ({items, isLoading}) => {
-  let arrayImg = items.map(item => {
-    <div className='card'>
-      <img src={item['img']} alt=""/>
-    </div>
-  })
 
   return (
     <Fragment>
@@ -23,24 +18,11 @@ const Hero = ({items, isLoading}) => {
           </div>
 
           <div className="cards">
-            <div className="card">
-            <img src="https://upload.wikimedia.org/wikipedia/en/thumb/0/03/Walter_White_S5B.png/220px-Walter_White_S5B.png" alt=""/>
-            </div>
-            <div className="card">
-              <img src="https://vignette.wikia.nocookie.net/breakingbad/images/9/95/JesseS5.jpg/revision/latest?cb=20120620012441" alt=""/>
-            </div><div className="card">
-            </div><div className="card">
-            </div><div className="card">
-            </div><div className="card">
-            </div><div className="card">
-            </div><div className="card">
-            </div><div className="card">
-            </div><div className="card">
-            </div><div className="card">
-            </div><div className="card">
-            </div><div className="card">
-            </div>
-            
+            {items.map(item => (
+              <div className="card">
+                <img src={item['img']} alt=""/>
+              </div>
+            ))}            
           </div>
         </div>
         </div>
